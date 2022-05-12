27f08813595578421986edd39b002c74e4e6ddb0
unknown <rtmissio@gmail.com>
2022-05-11 22:56:18 -0300

    creating project

---
+++ src/main/java/com/missio/wallet/WalletApplication.java
@@ -0,0 +1,13 @@
+package com.missio.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletApplication.class, args);
+	}
+
+}
