d5c1dfe290d897314b47d23dc07f91b253ba520f
Rafael Missio <rtmissio@gmail.com>
2022-05-11 23:21:31 -0300

    configuration initial the tests

--- src/test/java/com/missio/wallet/HelloWorld.java
+++ src/test/java/com/missio/wallet/HelloWorld.java
@@ -1,18 +1,18 @@
 package com.missio.wallet;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("test")
-class WalletApplicationTests {
+public class HelloWorld {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    public void testHelloWord(){
+        assertEquals(1,1);
+    }
 }
